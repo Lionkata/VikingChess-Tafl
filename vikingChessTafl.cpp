3409eb6bcb688e038a98350375b38ec986b11175
Leonoraaa <leonor.yankova1@gmail.com>
2025-01-28 00:46:15 +0200

    Update vikingChessTafl.cpp

--- vikingChessTafl.cpp
+++ vikingChessTafl.cpp
@@ -164,12 +164,11 @@ void printBoard(char** const board, int boardSize)
     }
     cout << endl;
 }
-
 void printRules()
 {
     cout << "Rules of Viking Chess game!" << endl;
     cout << "1. The attacking side moves first. " << endl;
-    cout << "2.All pieces move like the rook in Chess – in a straight line for as many" ;
+    cout << "2.All pieces move like the rook in Chess â€“ in a straight line for as many" ;
         cout << "empty squares as the player chooses." << endl;
     cout << "3. Pieces cannot hop over other pieces and cannot move diagonally." << endl;
     cout << "4. Capturing only happens when a piece is moved so that a single opposing ";
@@ -182,7 +181,7 @@ void printRules()
     cout << "8. A piece can safely move to a square between two of the opponent's pieces without being captured." << endl;
     cout << "9. The King is harder to capture because opposing pieces must surround him on all four sides or," ;
         cout << "if the King is at the edge of the board, on the 3 sides available. "<< endl;
-    cout << "10. Only the King is allowed into the corner squares – and, of course, such a move wins the game." << endl;
+    cout << "10. Only the King is allowed into the corner squares â€“ and, of course, such a move wins the game." << endl;
     cout << endl;
 }
 void printHelp()
@@ -197,186 +196,7 @@ void printHelp()
 
 
 
-bool areFigureCoordinatesValid(char** const board, int boardSize, int row, int colmn, bool playerAttack)
-{
-    if (row > boardSize || colmn > boardSize || row < 1 || colmn < 1)
-    {
-        return false;
-    }
-    if (board[row][colmn] == ATTACKER_SYMBOL && playerAttack)
-    {
-        return true;
-    }
-    else if ((board[row][colmn] == DEFENDER_SYMBOL || board[row][colmn] == KING_SYMBOL) && !playerAttack)
-    {
-        return true;
-    }
-    return false;
-}
-bool areMoveCoordinatesValid(char** const board, int boardSize, int row1, int colmn1, int row2, int colmn2, bool playerAttack)
-{
-    if (row2 > boardSize || colmn2 > boardSize || row2 < 1 || colmn2 < 1)
-    {
-        return false;
-    }
 
-    if (playerAttack)
-    {
-        if (board[row2][colmn2] != BLANK_SYMBOL)
-        {
-            return false;
-        }
-        else if (row2 != row1 && colmn1 != colmn2)
-        {
-            return false;
-        }
-
-        if (row1 == row2 && colmn1 > colmn2)
-        {
-            for (int i = colmn1; i > colmn2; i--)
-            {
-                if (board[row2][i])
-                {
-                    return false;
-                }
-            }
-        }
-        if (row1 == row2 && colmn1 < colmn2)
-        {
-            for (int i = colmn1; i < colmn2; i++)
-            {
-                if (board[row2][i])
-                {
-                    return false;
-                }
-            }
-        }
-        if (row1 == row2 && colmn1 > colmn2)
-        {
-            for (int i = colmn1; i > colmn2; i--)
-            {
-                if (board[row2][i])
-                {
-                    return false;
-                }
-            }
-        }
-        if (row1 == row2 && colmn1 > colmn2)
-        {
-            for (int i = colmn1; i > colmn2; i--)
-            {
-                if (board[row2][i])
-                {
-                    return false;
-                }
-            }
-        }
-    }
-    return 0;
-}
-
-void move()
-{
-
-}
-
-void chooseAction(char** const board, int boardSize, bool playerAttack)
-{
-    string action;
-    cout << "Your next move! (move, help, rules, quit, info, back, quit): ";
-    
-    while (true)
-    {
-        cin >> action;
-        if (action == "move") 
-        {
-            int row1, colmn1, row2, colmn2;
-            cout << "Figure coordinates: "; 
-            while (true)
-            {
-                cin >> row1 >> colmn1;
-                if (!areFigureCoordinatesValid(board, boardSize, row1, colmn1, playerAttack))
-                {
-                    cout << "Invalid coordinates!" << endl;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            cout << endl;
-            cout << "Move to: ";
-            while (true)
-            {
-                cin >> row2 >> colmn2;
-                if (!areMoveCoordinatesValid(board, boardSize, row2, colmn2, playerAttack))
-                {
-                    cout << "Invalid coordinates!" << endl;
-                }
-                else
-                {
-                    break;
-                }
-            }
-            move();
-            break;
-        }
-        else if (action == "help") {
-            printHelp();
-        }
-        else if (action == "rules" || action == "rule") {
-            printRules(); 
-        }
-        
-        else if (action == "info") {
-          // whoIsTurn();
-        }
-        else if (action == "back") {
-           // lastMoveList();
-        }
-        else if (action == "quit") {
-            break;
-        }
-        else {
-            cout << "Try again!";
-        }
-        cout << "Your next move! (move, help, rules, quit, info, back, quit): ";
-    } 
-}
-
-void switchPlayers(bool &playerAttack)
-{
-    if (playerAttack)
-    {
-        playerAttack = false;
-    }
-    else
-    {
-        playerAttack = true;
-    }
-}
-
-bool isGameOver(char** const board, int boardSize)
-{
-    int kingRow = -1, kingCol = -1;
-    for (int i = 0; i < boardSize; i++) 
-    {
-        for (int j = 0; j < boardSize; j++) 
-        {
-            if (board[i][j] == KING_SYMBOL) 
-            {
-                kingRow = i;
-                kingCol = j;
-                break;
-            }
-        }
-    }
-    return (kingRow == 0 && kingCol == 0) ||
-           (kingRow == 0 && kingCol == boardSize - 1) ||
-           (kingRow == boardSize - 1 && kingCol == 0) ||
-           (kingRow == boardSize - 1 && kingCol == boardSize - 1);
-   
-}
 
 int main()
 {
@@ -389,15 +209,7 @@ int main()
     initializeBoard(board, boardSize);
     putFiguresOnBoard(board, boardSize);
 
-    bool playerAttack = true;
-    while (isGameOver(board, boardSize))
-    {
-        printBoard(board, boardSize);
-        chooseAction(board, boardSize, playerAttack);
-
-        switchPlayers(playerAttack);
-    }
-
+   
 
 
     
